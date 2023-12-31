376525ebb76e2ade57ddad834cbbcc5766651911
root <root@DESKTOP-54DS7J3>
2023-12-31 19:03:12 +0100

    Dodano panel menu i opcję dodawania tytułu oraz treści notatki

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 notes = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        note = {'title': title, 'content': content}
+        notes.append(note)
+        return redirect(url_for('index'))  # Przeładuj stronę po dodaniu notatki
     return render_template('index.html', notes=notes)
 
-@app.route('/add_note', methods=['POST'])
-def add_note():
-    note = request.form['note']
-    notes.append(note)
-    return redirect(url_for('index'))
+@app.route('/note/<title>')
+def view_note(title):
+    # Kod do wyświetlania pojedynczej notatki
+    return render_template('note.html', title=title)  # Przykładowe wyświetlenie notatki na innej stronie
 
 if __name__ == '__main__':
     app.run(debug=True)
+
