5c073e687d9ee9a7f1cb71d21760b21f44545770
root <root@DESKTOP-54DS7J3>
2023-12-31 19:39:12 +0100

    Poprawiono opcję dodawania notatek - w tym celu usunięto formularz z głownego pliku index.html i stworzono dodatkowe pliki note i new_note. Stworzono odpowiednie funkcje w pliku app.py

--- app.py
+++ app.py
@@ -4,21 +4,29 @@ app = Flask(__name__)
 
 notes = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html', notes=notes)
+
+@app.route('/note/<title>')
+def view_note(title):
+    note = next((n for n in notes if n['title'] == title), None)
+    
+    if note:
+        return render_template('note.html', note=note)
+    else:
+        return "Note not found", 404
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
         note = {'title': title, 'content': content}
         notes.append(note)
         return redirect(url_for('index'))  # Przeładuj stronę po dodaniu notatki
-    return render_template('index.html', notes=notes)
-
-@app.route('/note/<title>')
-def view_note(title):
-    # Kod do wyświetlania pojedynczej notatki
-    return render_template('note.html', title=title)  # Przykładowe wyświetlenie notatki na innej stronie
-
+    return render_template('new_note.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
