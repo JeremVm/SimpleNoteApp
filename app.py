47b4d56326b4020b5bb69e1e84a2ba8ec2115bd7
root <root@DESKTOP-54DS7J3>
2023-12-31 18:03:55 +0100

    Utworzono podstawową aplikację Flask do notatek

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+notes = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', notes=notes)
+
+@app.route('/add_note', methods=['POST'])
+def add_note():
+    note = request.form['note']
+    notes.append(note)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
